95dea9fbccb9c8d16b2ddb2e0eb3abf56f49477b
Shadrack5 <shadrackkibet100@gmail.com>
2023-06-13 13:04:38 +0300

    added a js and styles

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var tablinks=document.getElementsByClassName("tab-links");
-var tabcontents=document.getElementsByClassName("tab-contents");
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link")
-    
-}
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    // Get form data
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    // Perform form validation and submission logic here
+    // For example, you can use an AJAX request to send the form data to a server-side script
+
+    // Reset the form after submission
+    document.getElementById("myForm").reset();
+});
