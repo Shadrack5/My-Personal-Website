421fa8061bb967e0da6abcab8876bfbb1805b4da
Shadrack5 <shadrackkibet100@gmail.com>
2023-06-13 14:37:47 +0300

    added redaame

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 function sendMessage(event) {
     event.preventDefault();
 
-    // Retrieve form data
+    
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
 
-    // Perform validation and submission logic
-    // For example, you can use AJAX to send the form data to a server-side script
-
-    // Reset the form after submission
     document.getElementById("myForm").reset();
 }
 
 function experience() {
-    // Handle the "Experience" tab click event
-    // Add your code here
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("experience").style.display = "block";
+    document.getElementById("education").style.display = "none";
 }
 
-function education() {
-    // Handle the "Education" tab click event
-    // Add your code here
-}
 
-function opentab(tabName) {
-    // Handle the tab switching l
-}
