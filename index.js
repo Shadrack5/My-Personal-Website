ebf23726e93b87d0f53c6be9d3a84fa7ab189d95
Shadrack5 <shadrackkibet100@gmail.com>
2023-06-13 14:17:01 +0300

    added a js and styles

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting
+function sendMessage(event) {
+    event.preventDefault();
 
-    // Get form data
+    // Retrieve form data
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
 
-    // Perform form validation and submission logic here
-    // For example, you can use an AJAX request to send the form data to a server-side script
+    // Perform validation and submission logic
+    // For example, you can use AJAX to send the form data to a server-side script
 
     // Reset the form after submission
     document.getElementById("myForm").reset();
-});
+}
+
+function experience() {
+    // Handle the "Experience" tab click event
+    // Add your code here
+}
+
+function education() {
+    // Handle the "Education" tab click event
+    // Add your code here
+}
+
+function opentab(tabName) {
+    // Handle the tab switching l
+}
